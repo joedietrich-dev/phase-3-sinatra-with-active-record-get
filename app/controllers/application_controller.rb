349763d98d80f1690c8aba93fe6367f3aa1868c9
joedietrich-dev <joe@joedietrich.net>
2021-11-26 11:24:14 -0500

    commit tree 65f5e54a6cb4bc2b08bcd49550ef7fee6289e43c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  set :default_content_type, 'application/json'
+
+  get '/games/:game_id' do
+    begin
+      game = Game.find(params[:game_id])
+    rescue
+      game = []
+    end
+    game.to_json({ only: [:id, :title, :genre, :price], 
+                   include: { reviews: { only: [:comment, :score], 
+                                         include: { user: { only: [:name] }}
+                                        }
+                            }
+                  })
+  end
+
+  get '/games' do
+    Game.all.order(:title).to_json
   end
 
 end
